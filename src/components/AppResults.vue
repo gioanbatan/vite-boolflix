b01d25e0dad443a1dbbbcae538b6491c110d3e8b
gioanbatan <giovannibassani@hotmail.com>
2022-11-14 13:26:25 +0100

    Add: Testing between AppResult/AppSingleResult/Store.js

--- src/components/AppResults.vue
+++ src/components/AppResults.vue
@@ -1,11 +1,23 @@
 <script>
+import AppSingleResult from "./AppSingleResult.vue";
+import { store } from "../store";
+
 export default {
     name: "AppResults",
+    components: {
+        AppSingleResult,
+    },
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 
 <template>
     TEST APPRESULTS
+    <AppSingleResult :result="store.testSingleResult" />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppSingleResult.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppSingleResult",
+    props: {
+        result: Object,
+    }
+}
+</script>
+
+<template>
+    <section>
+        <div>{{ result.title }}</div>
+        <div>{{ result.originalTitle }}</div>
+        <div>{{ result.language }}</div>
+        <div>{{ result.vote }}</div>
+    </section>
+</template>
+
+<style lang="scss">
+
+</style>
