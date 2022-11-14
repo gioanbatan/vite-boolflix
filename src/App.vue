fefec27268bb82746bffb9691ed57831b8ebec53
gioanbatan <giovannibassani@hotmail.com>
2022-11-14 12:20:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template>
+  <div>{{ store.test }}</div>
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    test: "Hello store!",
+    apiURL: "https://api.themoviedb.org/3/search/",
+    movieURL: "movie",
+    seriesURL: "tv",
+    apiKey: "2c49be34bbee907783d1de30dbc7df96"
+    query: "",
+    isLoading: false,
+    movieFound: [],
+    seriesFound: []
+});
